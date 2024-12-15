a0e4a8dad53aff32dd3f51eab70350fcc25de0e4
Speedy Consoles <>
2024-12-15 15:13:26 +0100

    Add code for day 14

---
+++ 2024/src/fourteen.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+use regex::Regex;
+
+type Vector = crate::vector::Vector<i32, 2>;
+
+pub const WIDTH: i32 = 101;
+pub const HEIGHT: i32 = 103;
+
+pub struct Robot {
+    pub position: Vector,
+    pub velocity: Vector,
+}
+
+pub fn parse_input() -> impl Iterator<Item=Robot> {
+    let regex = Regex::new(r"p=(\d+),(\d+) v=(-?\d+),(-?\d+)").unwrap();
+    io::stdin().lines()
+        .map(move |l| {
+            let [px, py, vx, vy]: [i32; 4] = regex.captures(&l.unwrap())
+                .unwrap()
+                .iter()
+                .skip(1)
+                .map(|m| m.unwrap().as_str().parse().unwrap())
+                .collect::<Vec<_>>()
+                .try_into()
+                .unwrap();
+            Robot {
+                position: Vector::new(px, py),
+                velocity: Vector::new(vx, vy),
+            }
+        })
+}
