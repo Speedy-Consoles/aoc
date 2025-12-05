1fc9935d378cb1daa996c701330cc617c7654079
Speedy Consoles <>
2025-12-05 21:28:53 +0100

    Fix indentation

--- 2025/src/two.rs
+++ 2025/src/two.rs
@@ -6,11 +6,11 @@ pub fn part_1(mut input: Box<dyn BufRead>) -> String {
     let mut sum = 0;
     for range_string in line.trim().split(",") {
         let [start, end] = range_string
-        .split('-')
-        .map(|id_string| id_string.parse::<i64>().unwrap())
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
+            .split('-')
+            .map(|id_string| id_string.parse::<i64>().unwrap())
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
         for id in start..end + 1 {
             let id_string = id.to_string();
             let invalid = id_string[..id_string.len() / 2] == id_string[id_string.len() / 2..];
@@ -28,11 +28,11 @@ pub fn part_2(mut input: Box<dyn BufRead>) -> String {
     let mut sum = 0;
     for range_string in line.trim().split(",") {
         let [start, end] = range_string
-        .split('-')
-        .map(|id_string| id_string.parse::<i64>().unwrap())
-        .collect::<Vec<_>>()
-        .try_into()
-        .unwrap();
+            .split('-')
+            .map(|id_string| id_string.parse::<i64>().unwrap())
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
         for id in start..end + 1 {
             let id_string = id.to_string();
             for n in 1..id_string.len() / 2 + 1 {
